f99dc1b941327b15d4a1699630832a355b1dbdfa
YonePC <YonePC@YONE>
2017-12-30 16:02:35 +0000

    clicking stars rating makes a console log

--- APM/src/app/shared/star.component.ts
+++ APM/src/app/shared/star.component.ts
@@ -7,10 +7,14 @@ import {Component, Input, OnChanges, SimpleChanges} from "@angular/core";
 })
 
 export class StarComponent implements OnChanges {
+  @Input() rating: number;
+  starWidth: number;
+
   ngOnChanges(changes: SimpleChanges): void {
     this.starWidth = this.rating * 86 / 5;
   }
 
-  @Input() rating: number;
-  starWidth: number;
+  onClick(): void {
+    console.log(`The rating ${this.rating} was clicked!`);
+  }
 }
