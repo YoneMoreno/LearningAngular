961c6722ea6e14f89831a3a31503e3a1b8e6f500
YonePC <YonePC@YONE>
2017-12-30 15:37:30 +0000

    created star component

---
+++ APM/src/app/shared/star.component.ts
@@ -0,0 +1,16 @@
+import {Component, OnChanges, SimpleChanges} from "@angular/core";
+
+@Component({
+  selector: 'pm-star',
+  template: './star.component.html',
+  styleUrls: ['./star.component.css']
+})
+
+export class StarComponent implements OnChanges{
+  ngOnChanges(changes: SimpleChanges): void {
+    this.startWidth = this.rating * 86/5;
+  }
+
+  rating: number=4;
+  startWidth: number;
+}
