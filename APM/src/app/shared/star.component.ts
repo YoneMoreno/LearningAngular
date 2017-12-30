c1df60b0fba94cb02917c351660a9430546b8898
YonePC <YonePC@YONE>
2017-12-30 15:48:53 +0000

    added star images to display graphically the rating

--- APM/src/app/shared/star.component.ts
+++ APM/src/app/shared/star.component.ts
@@ -2,15 +2,15 @@ import {Component, OnChanges, SimpleChanges} from "@angular/core";
 
 @Component({
   selector: 'pm-star',
-  template: './star.component.html',
+  templateUrl: './star.component.html',
   styleUrls: ['./star.component.css']
 })
 
-export class StarComponent implements OnChanges{
+export class StarComponent implements OnChanges {
   ngOnChanges(changes: SimpleChanges): void {
-    this.startWidth = this.rating * 86/5;
+    this.startWidth = this.rating * 86 / 5;
   }
 
-  rating: number=4;
+  rating: number = 4;
   startWidth: number;
 }
