836b88e389305ba12d23f42f457b4e54bf9698f2
YonePC <YonePC@YONE>
2017-12-30 08:32:39 +0000

    added class to use the template for product-list

---
+++ APM/src/app/products/product-list.component.ts
@@ -0,0 +1,10 @@
+import {Component} from "@angular/core";
+
+@Component({
+  selector: 'pm-products',
+  templateUrl: 'APM/src/app/products/product-list.component.html'
+})
+
+export class ProductListComponent {
+
+}
