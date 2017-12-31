f2b2b04bcd876231c0e541d0649282724c7665ae
YonePC <YonePC@YONE>
2017-12-31 17:01:21 +0000

    added routes to index nav anchors

--- APM/src/app/app.component.ts
+++ APM/src/app/app.component.ts
@@ -9,8 +9,8 @@ import {ProductService} from "./products/product.service";
         <div class="container-fluid">
           <ul class="nav navbar-nav">
             <li><a>{{pageTitle}}</a></li>
-            <li><a>Home</a></li>
-            <li><a>Product List</a></li>
+            <li><a [routerLink]="['/welcome']">Home</a></li>
+            <li><a [routerLink]="['/products']">Product List</a></li>
           </ul>
         </div>
       </nav>
